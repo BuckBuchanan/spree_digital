458125684b773e2183b33ad42a59fe8ca5d727a0
Robert Buchanan <robert@Roberts-MacBook-Pro.local>
2015-01-15 16:00:58 -0600

    Showing admin reset downloads button even when only some line-items have digitals

--- app/overrides/add_reset_digitals_to_admin_orders.rb
+++ app/overrides/add_reset_digitals_to_admin_orders.rb
@@ -2,6 +2,6 @@ Deface::Override.new(:virtual_path => "spree/admin/shared/_order_tabs",
                      :name => "add_reset_digitals_to_admin_orders",
                      :insert_bottom => "[data-hook='admin_order_tabs'], #admin_order_tabs[data-hook]",
                      :text => %q{
-<%= content_tag(:li, button_link_to(t(:reset_downloads), reset_digitals_admin_order_url(@order))) if @order.digital? %>
+<%= content_tag(:li, button_link_to(t(:reset_downloads), reset_digitals_admin_order_url(@order))) if @order.some_digital? %>
                       },
                      :disabled => false)
